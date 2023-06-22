84f6cbb4bfc6e5d91d65886093800045d8619df0
Stanislav Kosáček <kosacek@brilo.cz>
2023-06-22 20:14:11 +0200

    ukol 2.6 - bonus

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './style.css';
 
 const { pathname } = window.location;
 
-if (pathname === '/') {
+if (pathname === BASE_PATH + '/') {
   document.querySelector('#app').append(HomePage());
-} else if (pathname === '/objednavka') {
+} else if (pathname === BASE_PATH + '/objednavka') {
   document.querySelector('#app').append(OrderPage());
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const webpack = require('webpack');
+
+const publicPath =
+  process && process.env && process.env.GITHUB_REPOSITORY
+    ? '/' + process.env.GITHUB_REPOSITORY.split(`/`)[1]
+    : '';
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   devtool: 'eval-source-map',
   output: {
     filename: 'bundle-[contenthash:6].js',
-    publicPath: '/',
+    publicPath: publicPath + '/',
     clean: true,
   },
   devServer: {
@@ -36,6 +43,9 @@ module.exports = {
     }),
     new CopyPlugin({
       patterns: [{ from: 'public', to: '', noErrorOnMissing: true }],
+    }),
+    new webpack.DefinePlugin({
+      BASE_PATH: "'" + publicPath + "'",
     }),
   ],
 };
