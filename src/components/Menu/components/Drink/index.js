4aa0a0c4e9e223fd7f0d15297b83640504e8d5ea
Stanislav Kosáček <kosacek@brilo.cz>
2023-06-22 19:15:34 +0200

    ukol 2.2

--- src/components/Menu/components/Drink/index.js
+++ src/components/Menu/components/Drink/index.js
@@ -19,6 +19,32 @@ export const Drink = ({ id, name, ordered, image, layers }) => {
     <button class="order-btn">Objednat</button>
   </div>
   `;
+  const orderBtn = element.querySelector('.order-btn');
+
+  if (ordered) {
+    orderBtn.textContent = 'Zrušit';
+    orderBtn.classList.add('order-btn--ordered');
+  } else {
+    orderBtn.textContent = 'Objednat';
+    orderBtn.classList.remove('order-btn--ordered');
+  }
+
+  orderBtn.addEventListener('click', () => {
+    fetch(`https://cafelora.kodim.app/api/me/drinks/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+      body: JSON.stringify({
+        ordered: !ordered,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        element.replaceWith(Drink(data.result));
+      });
+  });
 
   const drinkInfoElm = element.querySelector('.drink__info');
   drinkInfoElm.append(...layers.map((layer) => Layer(layer)));
